a2b9e51f159911dc5280fa3b40cdf2942e86331d
DanBilic <bilic90@gmx.de>
2021-06-14 20:26:20 +0200

    corrected Event model

--- src/models/Event.js
+++ src/models/Event.js
@@ -3,20 +3,36 @@ import mongoose from "mongoose";
 const eventSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  createdAt: { type: Date, default: Date.now, required: true },
-  date: { type: Date, required: true },
-  admins: [{ type: [mongoose.Schema.ObjectId], ref: "User", required: true }],
+  createdAt: { type: Date, default: Date.now },
+  date: { type: Date, required: [true, "Please add a date"] },
+  admins: [
+    {
+      type: [mongoose.Schema.ObjectId],
+      ref: "User",
+      required: [true, "Please add an admin to your event!"],
+    },
+  ],
   invitedUsers: [
-    { type: [mongoose.Schema.ObjectId], ref: "User", required: true },
+    {
+      type: [mongoose.Schema.ObjectId],
+      ref: "User",
+      required: [true, "Please invite users to your event!"],
+    },
   ],
-  acceptedUsers: [
-    { type: [mongoose.Schema.ObjectId], ref: "User", required: true },
-  ],
+  acceptedUsers: [{ type: [mongoose.Schema.ObjectId], ref: "User" }],
   location: [
-    { type: [mongoose.Schema.ObjectId], ref: "Location", required: true },
+    {
+      type: [mongoose.Schema.ObjectId],
+      ref: "Location",
+      required: [true, "Please add a location"],
+    },
   ],
   komyuniti: [
-    { type: [mongoose.Schema.ObjectId], ref: "User", required: true },
+    {
+      type: [mongoose.Schema.ObjectId],
+      ref: "User",
+      required: [true, "Please add a Komyuniti"],
+    },
   ],
 });
 
